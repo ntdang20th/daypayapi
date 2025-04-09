e0c51e6074b10b93b7b97f40e109fc8f15166393
dang.nguyen <dang.nguyen@terralogic.com>
2025-04-09 14:56:40 +0700

    add test api

---
+++ src/DayPay.HttpApi/Controllers/TestController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DayPay.Controllers;
+
+[Route("test")]
+public sealed class TestController() : DayPayController
+{
+    [HttpGet]
+    public ActionResult<int> GetRandom() => Ok(new Random().Next(1, 10));
+}
